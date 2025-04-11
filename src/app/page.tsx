656986135be32c4e9f0510c3b555102969b4c91f
Ronald Labrador <ronlabdev@gmail.com>
2025-04-11 12:57:57 -0500

    Add chadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main>
+    <main className="w-screen h-screen flex flex-col items-center justify-center gap-20">
       <h1 className="">Grayola Test</h1>
     </main>
   );
